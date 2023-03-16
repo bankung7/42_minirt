1bf45ae74bcf31c3eceab283712829fd45ac90d3
golfzaa7 <vorravoot_n@hotmail.com>
2023-03-16 22:16:19 +0700

    base file

---
+++ minirt/incs/minirt.h
@@ -0,0 +1,10 @@
+#ifndef MINIRT_H
+#define MINIRT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "vector.h"
+#include "libft.h"
+
+#endif
---
+++ minirt/srcs/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "minirt.h"
+
+int main(void)
+{
+    printf("Welcome to RayTracer\n");
+    
+    return (0);
+}
